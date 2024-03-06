29650f858f3c9fe3c4d2c7876337df32ae55ab45
idoski <valaniftbuisness@gmail.com>
2024-03-06 14:07:25 -0800

    i removed a space

--- app/src/main/java/tutorial/App.java
+++ app/src/main/java/tutorial/App.java
@@ -5,7 +5,7 @@ package tutorial;
 
 public class App {
     public String getGreeting() {
-        return "The jake function";
+        return "The jakefunction";
     }
 
     public static void main(String[] args) {
