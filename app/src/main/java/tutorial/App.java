cf5cda2b6f4236f158db219987af788c19dc04ae
idoski <valaniftbuisness@gmail.com>
2024-03-06 13:14:10 -0800

    i changed the func
    i did it!

--- app/src/main/java/tutorial/App.java
+++ app/src/main/java/tutorial/App.java
@@ -5,7 +5,7 @@ package tutorial;
 
 public class App {
     public String getGreeting() {
-        return "Goodbye Space";
+        return "The jake function";
     }
 
     public static void main(String[] args) {
